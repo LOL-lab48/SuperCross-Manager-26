1fe38ac6db64e56aab5e0055c8430db4714474fc
LOL-lab48 <32gk@student.nbcs.nsw.edu.au>
2026-01-16 08:11:39 +1100

    Create riders.js

---
+++ src/data/riders.js
@@ -0,0 +1,11 @@
+const drivers = [
+  "Cooper Webb","Eli Tomac","Chase Sexton","Jett Lawrence","Jason Anderson",
+  "Aaron Plessinger","Dylan Ferrandis","Malcolm Stewart","Christian Craig",
+  "Justin Cooper","Adam Cianciarulo","Dean Wilson","Jorge Prado","Tom Vialle",
+  "RJ Hampshire","Nate Thrasher","Austin Forkner","Haiden Deegan","Jo Shimoda",
+  "Jeremy Martin","Kyle Chisholm","Ben Bloss","Shane McElrath","Grant Harlan",
+  "Seth Hammaker","Max Vohland","Chance Hymas","Julien Beaumer","Cole Davies",
+  "Nick Romano","Drew Adams","Ryder DiFrancesco","Hunter Lawrence",
+  "Michael Mosiman","Jordon Smith","Logan Karnow","Ty Masterpool",
+  "Parker Ross","Kevin Moranz","Carson Mumford"
+];
